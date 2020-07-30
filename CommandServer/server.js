413049a474ec26a257181193d9591dd49604f36b
Olsonrowan <56852849+Olsonrowan@users.noreply.github.com>
2020-07-30 13:38:00 -0600

    final commit
    added /help for instructions and fixed issues with kicking self and whispering self.

--- CommandServer/server.js
+++ CommandServer/server.js
@@ -1,126 +1,136 @@
-const server = net.createServer();
-const output = fs.createWriteStream('server.log');
-const net = require('net');
 const fs = require('fs');
+const net = require('net');
+const server = net.createServer();
+const createFile = fs.createWriteStream('server.log');
 let users = [];
 let count = 1;
-let admin = '12345';
+let password = '12345';
 
 
 server.listen(6000, () => log('Server up on port: 6000'));
 
 server.on('connection', socket => {
-
 	socket.id = count++;
 	socket.name = null;
 	users.push(socket);
-    socket.write('Welcome!!! type /quit to leave chat!');
+    socket.write('Welcome! type your username and hit enter to get started');
 	
 	socket.on('data', chunk => {
 		chunk = chunk.toString().trim();
 		if( socket.name === null){
-			//username
+			setUsername(socket, chunk)
 		} else if(chunk[0] === '/'){
-			//handle input commands
+			commands(socket, chunk);
 		} else {
-			log(`socket.name and chunk $$`)
-			//notify
+			log(`${socket.name}: ${chunk}`)
+			announce(socket.name, `${socket.name}: ${chunk}`);
 		}
 
 	})
 	.on('end', () => {
-		for (let i in users) {
-			if (users[i].id === socket.id) users.splice(i, 1);
-		}
-		log(`User ${socket.id} has disconnected`)
+		removeUser(socket.name)
+		})
 	})
-})
 
-function log(message) {
-	console.log(message);
-	output.write(message + '\n');
-}
+	function setUsername(socket,chunk){
+		socket.name = chunk;
+		if(users.length > 0) connectedUsers = clientlist(socket);
+		socket.write(`welcome ${socket.name}. type /quit to end session`)
+		connectMsg(socket);
+	}
 
+	function connectMsg(socket) {
+		let success = `${socket.name} has connected`;
+		announce(socket.name, success);
+		log(success);
+	}
 
+	function commands(socket, chunk) {
+		chunk = chunk.slice(1);
+		chunk = chunk.split(' ');
+		let command = chunk.shift();
+		let name = chunk.shift();
+		let message = chunk.join(' ');
+		// https://www.w3schools.com/js/js_switch.asp remember this
+		switch(command) {
+			case 'help':
+				socket.write('/w {name} "message"\n/username {new name}\n/kick {name} {admin password}\n/quit to exit chat session.')
+				break;
+			case 'w':
+				if(socket.name != name){
+				let whisper = socket.name + ' whispers: ' + message;
+				findUser(name).write(whisper);
+				log(whisper);
+				} else {
+					socket.write('you cannot whisper to yourself.')
+				}
+				break;
+			case 'username':
+				let oldName = socket.name;
+				socket.name = name;
+				socket.write('Username has be changed to: ' + socket.name);
+				let logMessage = `User (${oldName}) has changed their username to: ${socket.name}`;
+				announce(socket.name, logMessage);
+				log(logMessage);
+				break;
+			case 'kick':
+				if(message === password && socket.name != name) {
+					let kickedUser = findUser(name);
+					kickedUser.write('You have been removed from chat');
+					kickedUser.end();
+					removeUser(name);
+					let kickMessage = kickedUser.name + ' has been removed from the chat';
+					announce(kickedUser.name, kickMessage);
+					log(kickMessage);
+				}else if(socket.name = name){
+					 socket.write('You cannot /kick yourself! type /quit to exit chat');
+					 break;
+				}
+				else socket.write('Only users with the admin password may kick users!');
+				break;
+			case 'clientlist':
+				socket.write('Connected Users:\n');
+				socket.write(clientlist(socket));
+				break;
+			default:
+				socket.write('Unknown command, type /help for command options\n');
+				break;
+		}
+	}
+	
+	function findUser(username) {
+		for (let i in users) {
+			if(users[i].name === username) return users[i];
+		}
+		return null;
+	}
+	
+	function announce(username, message) {
+		for (let i in users) {
+			if (users[i].name !== username) users[i].write(message);
+		}
+	}
+	
+	function removeUser(name) {
+		for (let i in users) {
+			if (users[i].name === name) {
+				users.splice(i, 1);
+			}
+		}
+		log(`${name} has disconnected`);
+	}
+	
+	function clientlist(socket) {
+		connectedUsers = '';
+		for (let i in users) {
+			if(users[i].id !== socket.id) connectedUsers += users[i].name + ', ';
+		}
+		connectedUsers += socket.name;
+		return connectedUsers;
+	}
+	
+	function log(message) {
+		console.log(message);
+		createFile.write(message + '\n');
+	}
 
-
-
-// const net = require('net');
-// const fs = require('fs');
-// let user = 0;
-// let sockets = [];
-
-// let output = fs.createWriteStream('server.log')
-// let server = net.createServer(client => {
-
-//     user++;
-
-    
-//     client.userName = "user" + user;
-//     let userName = client.userName
-//     sockets.push(client);
-
-//     messages(userName, userName, + ' joined this chat.\n');
-
-//     console.log(userName + ' has connected\n');
-//     client.write(`Welcome to the chat room!\n`);
-
-//     client.on('data', data => {
-//        let message = userName + '> ' + data.toString().trim() + '\n';
-//        messages(userName, message);
-
-//        // Log it to the server output
-//        process.stdout.write(message);
-//     })
-//     // client.on('end', () => {
-//     //     console.log('userwhatever disconnected');
-//     // });
-
-
-
-//     function messages(from, message) {
-
-//         sockets.forEach(function(client, index, array){
-//             // Dont send any messages to the sender
-//             if(client.userName === from) return;
-            
-//             client.write(message);
-        
-//         });
-        
-//     };
-
-//     function removeUser(client) {
-
-//         sockets.splice(sockets.indexOf(client), 1);
-
-    
-//     };
-
-//     client.on('end', function() {
-
-// 		let quitMessage = userName + ' left this chat\n';
-
-// 		// Log it to the server output
-// 		process.stdout.write(quitMessage);
-
-// 		// Remove client from socket array
-// 		removeUser(client);
-
-// 		// Notify all clients
-// 		messages(userName, quitMessage);
-// 	});
-
-
-// 	// When socket gets errors
-// 	client.on('error', function(error) {
-
-// 		console.log('problems: ', error.message);
-
-// 	});
-
-
-
-
-// }).listen(5000);
-// console.log(`Listening on port ${server.address().port}`);
